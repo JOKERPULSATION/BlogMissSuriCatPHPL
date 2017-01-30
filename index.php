d54328dabcbb64e05268eef3129cb5ffcaa4a669
JOKERPULSATION <hamelinpaul@hotmail.fr>
2017-01-30 16:43:02 +0100

    script comandeV1

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ include_once('models/entities/Client.php');
 include_once('models/entities/User.php');
 include_once('models/repositories/ClientRepository.php');
 include_once('models/repositories/UserRepository.php');
-include_once('models/repositories/CommandeRepository.php')
+include_once('models/repositories/CommandeRepository.php');
 
 //On récupère un objet PDO une fois pour toutes pour dialoguer avec la bdd
 $pdo = PDOFactory::getMysqlConnection();
@@ -124,8 +124,8 @@ switch ($action) {
 
 	case "listCommande":
 		//On prépare la vue a afficher avec les données dont elle a besoin
-		$commandeRepo = new CommandeRepository.php();
-		$ListeCommande = $CommandeRepo->getAll($pdo);
+		$commandeRepo = new CommandeRepository();
+		$listeCommande = $commandeRepo->getAll($pdo);
 		$vueAAfficher = "views/listCommande.php";
 		break;
 
